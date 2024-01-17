b1c4756f02dd0a5607522b438d1adfa81310a380
Vinay Ahari <vinayhr5@gmail.com>
2024-01-17 22:44:44 +0530

    Updated Varibles

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Vinay Ahari",
   roles: [
     "Full Stack Developer",
     "Android Developer",
@@ -355,7 +355,7 @@ export const projects = [
     webapp: "https://podstream.netlify.app/",
     member: [
       {
-        name: "Rishav Chanda",
+        name: "Vinay Ahari,
         img: "https://avatars.githubusercontent.com/u/64485885?v=4",
         linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
         github: "https://github.com/rishavchanda/",
@@ -402,7 +402,7 @@ export const projects = [
     webapp: "https://brain-tumor.netlify.app/",
     member: [
       {
-        name: "Rishav Chanda",
+        name: "Vinay Ahari,
         img: "https://avatars.githubusercontent.com/u/64485885?v=4",
         linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
         github: "https://github.com/rishavchanda/",
